e3f126d16d52383c78dbbf9836ba47a461109e80
MohammadBarek <barekzai.noman@gmail.com>
2025-05-11 02:39:56 -0400

    Added prework files to repository

---
+++ Myproject3Tests/Myproject3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Myproject3Tests.swift
+//  Myproject3Tests
+//
+//  Created by Mohammad Noman Barekzai on 5/11/25.
+//
+
+import Testing
+@testable import Myproject3
+
+struct Myproject3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
